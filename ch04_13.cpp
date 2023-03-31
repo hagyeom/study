6cab15642afcddacf080576dc424bac10408e226
hagyeom <116890147+hagyeom@users.noreply.github.com>
2023-03-31 19:53:41 +0900

    Rename ch04_12.cpp to ch04_13.cpp

---
+++ ch04_13.cpp
@@ -0,0 +1,33 @@
+/* 문자열을 입력 받고 회전시키기 */
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string s;
+
+	cout << "아래에 문자열을 입력하세요. 빈 칸이 있어도 됩니다.(한글 안됨) " << endl;
+	getline(cin, s, '\n'); // 문자열 입력
+	int len = s.length(); // 문자열의 길이
+
+	for (int i = 0; i < len; i++) {
+		string first = s.substr(0, 1); // 맨 앞의 문자 1개를 문자열로 분리
+		string sub = s.substr(1, len - 1); // 나머지 문자들을 문자열로 분리
+		s = sub + first; // 두 문자열을 연결하여 새로운 문자열로 만듦
+		cout << s << endl;
+	}
+}
+/*
+아래에 문자열을 입력하세요. 빈 칸이 있어도 됩니다.(한글 안됨)
+I love you
+ love youI
+love youI
+ove youI l
+ve youI lo
+e youI lov
+ youI love
+youI love
+ouI love y
+uI love yo
+I love you
+*/
