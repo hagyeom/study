2eed59f20b2fbd139b10a5e9883c5b5f9beb5647
hagyeom <116890147+hagyeom@users.noreply.github.com>
2023-04-01 19:28:55 +0900

    Update ch03_training1.cpp

--- ch03_training1.cpp
+++ ch03_training1.cpp
@@ -25,5 +25,5 @@ int main() {
 	Tower myTower;
 	Tower seoulTower(100);
 	cout << "높이는 " << myTower.getHeight() << "미터" << endl;
-	cout << " " << seoulTower.getHeight() << "미터" << endl;
+	cout << "높이는 " << seoulTower.getHeight() << "미터" << endl;
 }
